45387023237fbb1ddc911a9589514da02a31c6e5
Mark Kauffman <markkauffman22@gmail.com>
2019-10-16 15:15:49 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def bark
+    puts bark "Woof"!
+  end  
+  def sit 
+    puts "The dog is sitting"
+  end  
+end 
+
+class Person 
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end  
+end  
+
